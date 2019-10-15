01198089a4185ced45e2d4d257eb5cd3c27874a6
cls vitolipari <vito.lipari.clsinformatica@gmail.com>
2019-10-15 15:17:08 +0200

    init

---
+++ src/main/java/com/liparistudios/webSocketSpringFluxTraining/WebSocketSpringFluxTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.liparistudios.webSocketSpringFluxTraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketSpringFluxTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketSpringFluxTrainingApplication.class, args);
+	}
+
+}
