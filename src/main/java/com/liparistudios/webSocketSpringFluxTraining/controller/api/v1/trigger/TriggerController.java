a9c751a7f300475a9fa35bd9c368cd0f2f38d210
cls vitolipari <vito.lipari.clsinformatica@gmail.com>
2019-10-16 15:14:05 +0200

    #wip

--- src/main/java/com/liparistudios/webSocketSpringFluxTraining/controller/api/v1/trigger/TriggerController.java
+++ src/main/java/com/liparistudios/webSocketSpringFluxTraining/controller/api/v1/trigger/TriggerController.java
@@ -3,7 +3,6 @@ package com.liparistudios.webSocketSpringFluxTraining.controller.api.v1.trigger;
 import com.liparistudios.webSocketSpringFluxTraining.controller.api.v1.BaseAPIController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,9 +13,9 @@ import java.util.Map;
 public class TriggerController extends BaseAPIController {
 
 
-    @PostMapping("/check-instagram")
+    @PostMapping("/action")
     public @ResponseBody
-    ResponseEntity<Map<String, Object>> checkInstagramUsername( @RequestBody String request ) throws IOException {
+    ResponseEntity<Map<String, Object>> trigger( @RequestBody String request ) throws IOException {
 
         Map<String, Object> userDataResponseMap = new HashMap<String, Object>(){{
             put("status", "ok");
