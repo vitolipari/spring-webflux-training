0ff6e84df83857d6e830358311e775dc2b9d2f61
cls vitolipari <vito.lipari.clsinformatica@gmail.com>
2019-10-21 18:46:23 +0200

    #wip

--- src/main/java/com/liparistudios/webSocketSpringFluxTraining/service/TriggerService.java
+++ src/main/java/com/liparistudios/webSocketSpringFluxTraining/service/TriggerService.java
@@ -31,29 +31,37 @@ public class TriggerService {
     public void broadcastEmitNotific() {
 
 
-        Flux<String> nameStream = Flux.fromIterable( new LinkedList<>(){{
-            add("uno");
-            add("due");
-            add("tre");
-            add("quattro");
-            add("cinque");
-            add("sei");
-            add("sette");
-            add("otto");
-        }});
+        Flux<String> nameStream =
+            Flux
+                .fromIterable( new LinkedList<String>(){{
+                    add("uno");
+                    add("due");
+                    add("tre");
+                    add("quattro");
+                    add("cinque");
+                    add("sei");
+                    add("sette");
+                    add("otto");
+                }})
+                .flatMap( streamElement -> {
+                    System.out.println("clockStream doOnNext");
+                    System.out.println( streamElement );
+                    return Mono.just( streamElement );
+                })
+        ;
 
-        Flux<Long> interval = Flux.interval(Duration.ofSeconds(1));
+        Flux<Long> interval = Flux.interval(Duration.ofMillis(500));
         Flux<String> clockStream = Flux.zip(nameStream, interval, (key, value) -> key);
+
+
 
         session.forEach( ses -> {
             System.out.println("emit per session ---> "+ ses.getId() );
 
             clockStream
                 .doOnNext( streamElement -> {
-                    System.out.println("clockStream doOnNext");
-                    System.out.println( streamElement );
                     try {
-                        ses.sendMessage(new TextMessage("TRIGGER "+ ses.getId()));
+                        ses.sendMessage(new TextMessage("TRIGGER "+ ses.getId() +" "+ streamElement));
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
