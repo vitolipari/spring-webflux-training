3b127d6f589357579f58161837e78cc5c5b062da
cls vitolipari <vito.lipari.clsinformatica@gmail.com>
2019-10-21 17:09:53 +0200

    #wip

--- src/main/java/com/liparistudios/webSocketSpringFluxTraining/service/TriggerService.java
+++ src/main/java/com/liparistudios/webSocketSpringFluxTraining/service/TriggerService.java
@@ -45,70 +45,8 @@ public class TriggerService {
         Flux<Long> interval = Flux.interval(Duration.ofSeconds(1));
         Flux<String> clockStream = Flux.zip(nameStream, interval, (key, value) -> key);
 
-//        clockStream
-//            .doOnNext( streamElement -> {
-//                System.out.println("clockStream doOnNext");
-//                System.out.println( streamElement );
-//            })
-//            .subscribe()
-//        ;
-
-
-        Flux<Object> flux = Flux
-            .create( emitter -> {
-                int i = 0;
-                while ( i < 10 ) {
-                    int finalI = i;
-                    emitter.next(new HashMap<String, Object>(){{
-                        put(String.valueOf(finalI), (new Date()).getTime() );
-                    }} );
-                    i++;
-                }
-            })
-            .publish()
-            .autoConnect()
-        ;
-
-        flux
-            .flatMap( streamElement -> Mono.just( streamElement ) )   // asincrono per questo invexce che ritornare un elemento torna un wrap di tipo Mono all'elemento
-            .doOnNext( streamElement -> {
-                System.out.println("flux doOnNext");
-                System.out.println( streamElement );
-            })
-//            .subscribe()
-        ;
-
-
-
-
-            /*
-        session
-            .stream()
-            .map( ses -> {
-                clockStream.next();
-                return ses;
-            })
-            .map( ses -> {
-                System.out.println("current session: "+ ses.getId());
-                flux.next();
-                return ses;
-            })
-            .map( ses -> {
-                try {
-                    System.out.println("send to ---> "+ ses.getId() );
-                    ses.sendMessage(new TextMessage("TRIGGER "+ ses.getId()));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return ses;
-            })
-        ;
-            */
-
-
         session.forEach( ses -> {
             System.out.println("emit per session ---> "+ ses.getId() );
-
 
             clockStream
                 .doOnNext( streamElement -> {
@@ -122,8 +60,6 @@ public class TriggerService {
                 })
                 .subscribe()
             ;
-
-//                    ses.sendMessage(new TextMessage("TRIGGER "+ ses.getId()));
 
         });
 
